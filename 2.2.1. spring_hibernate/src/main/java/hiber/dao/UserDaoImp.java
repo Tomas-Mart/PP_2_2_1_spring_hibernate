bebbc2c85fdf2ca5edfe4830f8bff786a40c76a9
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-12 00:25:33 +0400

    Hibernate и ORM

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,6 +4,7 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -11,19 +12,33 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    public User getUserByCarModelAndSeries(String model, int series) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User WHERE car.model = :model AND car.series = :series", User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        return query.getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public void clearUsersAndCars() {
+        sessionFactory.getCurrentSession().createQuery("DELETE FROM User").executeUpdate();
+        sessionFactory.getCurrentSession().createQuery("DELETE FROM Car").executeUpdate();
+    }
 }
